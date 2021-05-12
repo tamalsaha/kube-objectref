c0e2feeb6e0b9c54865bf217a15b535ac8f1b1de
Tamal Saha <tamal@appscode.com>
2021-05-12 11:32:53 -0700

    added-all

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"log"
-	"path/filepath"
-
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery/cached/memory"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	"log"
+	"path/filepath"
+	"kmodules.xyz/resource-metadata/apis/meta/v1alpha1"
 )
+
+type ObjectLocator struct {
+	Start StartObjectRef `json:"start"`
+	Path []v1alpha1.ResourceConnection         `json:"connections,omitempty"`
+}
+
+type StartObjectRef struct {
+	Target       metav1.TypeMeta       `json:"target"`
+	// Namespace always same as Workflow
+	Selector     *metav1.LabelSelector `json:"selector,omitempty"`
+	NameTemplate string                `json:"nameTemplate,omitempty"`
+}
 
 func main() {
 	masterURL := ""
