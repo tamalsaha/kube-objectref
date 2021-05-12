cd220525c041c7951d85f4fb461f6b3d967adbef
Tamal Saha <tamal@appscode.com>
2021-05-12 00:34:08 -0700

    added-all

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"log"
+	"path/filepath"
 
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/client-go/discovery/cached/memory"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	masterURL := ""
+	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfigPath)
+	if err != nil {
+		log.Fatalf("Could not get Kubernetes config: %s", err)
+	}
+
+	client := kubernetes.NewForConfigOrDie(config)
+
+	var mapper meta.RESTMapper
+	mapper = restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(client.Discovery()))
+
+	gvr := schema.GroupVersionResource{
+		Group:    "admissionregistration.k8s.io",
+		Version:  "",
+		Resource: "mutatingwebhookconfigurations",
+	}
+	gvrs, err := mapper.ResourcesFor(gvr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(gvrs)
 }
